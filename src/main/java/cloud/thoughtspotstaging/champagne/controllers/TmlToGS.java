0350ba7595414cddbac332327a6bfc631beb818b
Shaik ansari <shaik.ansari@thoughtspot.com>
2021-08-21 20:58:30 +0530

    test classes

--- src/main/java/cloud/thoughtspotstaging/champagne/controllers/TmlToGS.java
+++ src/main/java/cloud/thoughtspotstaging/champagne/controllers/TmlToGS.java
@@ -12,7 +12,6 @@ public class TmlToGS {
     public static void main(String[] args) {
 
         try {
-            //System.out.println(UUID.fromString("59481331-ee53-42be-a548-bd87be6ddd4b"));
             String filePathWorksheet = "/Users/shaik.ansari/Downloads/Test-Worksheet.worksheet/Test-Worksheet.worksheet.tml";
             //String tablePath = "/Users/shaik.ansari/Downloads/Test-Worksheet.worksheet/ORDERS.table.tml";
             String tablePath = "/Users/shaik.ansari/Downloads/Test-Worksheet.worksheet/CUSTOMER.table.tml";
